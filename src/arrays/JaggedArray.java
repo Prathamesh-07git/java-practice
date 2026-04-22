ba41a55f8487e1857ecccbff79c415c65b3bd025
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-04-22 12:24:16 +0530

    Jagged Array code added

---
+++ src/arrays/JaggedArray.java
@@ -0,0 +1,31 @@
+package arrays;
+
+
+
+    import java.util.Scanner;
+
+    public class JaggedArray {
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            int[][] arr = new int[3][];
+
+            arr[0] = new int[4];
+            arr[1] = new int[3];
+            arr[2] = new int[3];
+
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }

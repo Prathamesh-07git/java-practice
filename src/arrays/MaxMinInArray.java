126456888ecdbce949688f35f3c0713f61035a24
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-06-26 22:59:38 +0530

    size of the array

--- src/arrays/MaxMinInArray.java
+++ src/arrays/MaxMinInArray.java
@@ -1,4 +1,15 @@
+/*find the maximum and minimum in the array*/
 package arrays;
-
+import java.util.Scanner;
 public class MaxMinInArray {
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array:- ");
+        int size = sc.nextInt();
+
+
+
+    }
 }

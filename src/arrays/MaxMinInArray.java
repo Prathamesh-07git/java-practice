3e14fc9cdf1d4b5f060a31db984f21f654fa3498
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-06-26 23:48:48 +0530

    Maximum and minimum in the array using methods

--- src/arrays/MaxMinInArray.java
+++ src/arrays/MaxMinInArray.java
@@ -1,14 +1,57 @@
 /*find the maximum and minimum in the array*/
 package arrays;
+
 import java.util.Scanner;
+
 public class MaxMinInArray {
 
-    public static void main(String [] args){
+// method for find maximum  number in the array
+    public static int maxInTheArray(int[] arr) {
+
+        int max = arr[0];
+
+        //for maximum
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return max;
+
+    }
+// method for finding minimum number in the array
+
+    public static int minInTheArray(int[] arr){
+        int min = arr[0];
+
+        for(int i = 0;i<arr.length;i++){
+            if (arr[i]<min){
+                min =arr[i];
+            }
+        }
+        return min;
+    }
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the size of the array:- ");
         int size = sc.nextInt();
 
+
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(" " + arr[i]);
+        }
+        System.out.println();
+
+
+        System.out.println("The maximum element in the array is :- "+maxInTheArray(arr));
+        System.out.println("The minimum element in the array is :- "+minInTheArray(arr));
 
 
     }

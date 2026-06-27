8edb85d5ab6094edf961467f5dec806db8b22a9e
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-06-27 23:50:21 +0530

    matrix addition

---
+++ src/arrays/Matrix_addition.java
@@ -0,0 +1,27 @@
+
+package arrays;
+import java.util.Scanner;
+public class Matrix_addition {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Enter the value of rows :- ");
+        int rows =sc.nextInt();
+
+        System.out.print("Enter the value of rows :- ");
+        int columns=sc.nextInt();
+
+        int [][] arr1 = new int [rows][columns];
+
+
+        for(int i = 0;i<arr1.length;i++){
+            for (int j= 0;j<arr1.length;j++){
+                arr1[i][j]=sc.nextInt();
+            }
+        }
+
+
+    }
+
+}

a5c7f7ad7b7ec0395c6d2e925fa2757275688e0c
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-06-28 00:01:47 +0530

    Fix matrix traversal bug by replacing arr1.length with arr1[i].length

--- src/arrays/Matrix_addition.java
+++ src/arrays/Matrix_addition.java
@@ -16,12 +16,22 @@ public class Matrix_addition {
 
 
         for(int i = 0;i<arr1.length;i++){
-            for (int j= 0;j<arr1.length;j++){
+            for (int j= 0;j<arr1[i].length;j++){
                 arr1[i][j]=sc.nextInt();
             }
         }
 
 
+//        for(int[] matrixAdd : arr1){
+//            System.out.println(matrixAdd);
+//        }  gives garbage values
+
+for(int i = 0;i<arr1.length;i++){
+    for (int j =0;j<arr1[i].length;j++){
+        System.out.print(" "+arr1[i][j]);
+    }
+    System.out.println();
+}
     }
 
 }

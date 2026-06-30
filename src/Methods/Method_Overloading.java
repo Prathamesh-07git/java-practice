8a3860b3f6fac262d752e90f7f4c3f8045d7947c
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-06-30 17:25:46 +0530

    Method Overloading

--- src/Methods/Method_Overloading.java
+++ src/Methods/Method_Overloading.java
@@ -6,7 +6,7 @@ package Methods;
 //Creating multiple methods with the same name but different parameter lists in the same class.
 
 public class Method_Overloading {
-
+/*
  public static void print(int x){
      System.out.println(x);
  }
@@ -17,12 +17,27 @@ public class Method_Overloading {
  public static void print(String x){
      System.out.println(x);
  }
-
+*/
     public static void main(String [] args){
 
-     print(5);
+   /*  print(5);
      print(5.0);
      print("Prathamesh");
+    */
+
+
+        System.out.println(add(5,10));
+        System.out.println(add(5,10,15));
     }
+
+    public static int add(int a , int b){
+        return a+b;
+    }
+
+    public static int add(int a,int b, int c){
+        return a + b + c;
+    }
+
+
 
 }

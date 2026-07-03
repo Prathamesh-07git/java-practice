62b4ac6026e6708a124f55d679df75d655515257
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-07-03 12:06:16 +0530

    Fix Fibonacci series logic and handle edge cases

--- src/Methods/Fibonacci_Series.java
+++ src/Methods/Fibonacci_Series.java
@@ -1,29 +1,41 @@
 package Methods;
-//Q2. Write recursive method fibonacci(int n) that returns nth fibonacci number
+//Q2. Write  method fibonacci(int n) that returns nth fibonacci number by using loops
 
 // 0 1 1 2 3 5 8 ............
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Fibonacci_Series {
     public static void printFibonacci(int n){
         int prev = 0;
         int current = 1;
         int next;
-        System.out.println(prev);
-        System.out.println(current);
-        for(int i = 0;i<n;i++){
+
+        if (n<=0){
+            System.out.println("Invalid input ! please Enter the number Greater than 0 ");
+        }
+
+        else if(n==1){
+            System.out.print(prev);
+        }
+        else {
+            System.out.print(" "+prev);
+            System.out.print(" "+current);
+        }
+
+        for(int i = 0;i<n-2;i++){
 
             next = prev + current;
 
             prev = current;
             current = next;
-            System.out.println(next);
+            System.out.print(" "+next);
         }
 
     }
     public static void main(String [] args){
        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the number :- ");
+        System.out.print("Enter the number :- ");
         int nth = sc.nextInt();
 
 

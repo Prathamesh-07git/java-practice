ef8c63fce571df4d5f5f0f72fc37e4c558287f13
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-07-05 23:58:19 +0530

    Strings in java

---
+++ src/recursion/Power_Of_Number_By_Recursion.java
@@ -0,0 +1,4 @@
+package recursion;
+
+public class Power_Of_Number_By_Recursion {
+}
---
+++ src/strings/Strings_In_Java.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class Strings_In_Java {
+
+    public static void main(String[] args){
+
+      //By String literal
+       String name = "Prathamesh";
+
+        System.out.println(name);
+    }
+}

9392131124262a750b2bb662b4dfc4c67787a70e
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-07-06 23:56:11 +0530

    charAt() is use to print the string element

--- src/strings/Strings_In_Java.java
+++ src/strings/Strings_In_Java.java
@@ -1,12 +1,30 @@
 package strings;
-
+import java.util.Scanner;
 public class Strings_In_Java {
 
     public static void main(String[] args){
+
+
+        Scanner sc = new Scanner(System.in);
+
 
       //By String literal
        String name = "Prathamesh";
 
         System.out.println(name);
+
+        System.out.print("Enter your full name :- ");
+        String fullName = sc.nextLine();
+
+        System.out.println(fullName);
+
+
+        for(int i = 0;i<name.length();i++){
+            System.out.printc(name.charAt(i)+"@");
+        }
+
+
+
+
     }
 }

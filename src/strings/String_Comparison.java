beb5dba93f1c9a00157230a9da35adf7390fa7c4
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-07-07 23:48:57 +0530

    Add Java Strings concepts and practice examples

--- src/strings/String_Comparison.java
+++ src/strings/String_Comparison.java
@@ -1,7 +1,25 @@
 package strings;
 
+import javax.xml.transform.Source;
+
 public class String_Comparison {
     public static void main(String [] args){
+/*
+Now you need to check if both are the same.
+Java provides two ways:
+==
+.equals()
+These do different things.
+* */
+//  == checks whether two references point to the same object in memory.
+//It does not compare the actual characters.
+        String username = "Prathamesh";
+        String input = "Prathamesh";
 
+        System.out.println(username==input);
+        //.equals() compares the contents (characters) of two strings.
+        String a = "hi";
+        String b = "hello";
+        System.out.println(a.equals(b));
     }
 }

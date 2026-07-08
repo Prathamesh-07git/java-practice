27fb5d2d9ffb016aab5ff3138738b3e8968e9326
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-07-08 23:50:55 +0530

    Add Java Strings concepts and practice examples

--- src/strings/String_Comparison.java
+++ src/strings/String_Comparison.java
@@ -4,22 +4,33 @@ import javax.xml.transform.Source;
 
 public class String_Comparison {
     public static void main(String [] args){
-/*
-Now you need to check if both are the same.
-Java provides two ways:
-==
-.equals()
-These do different things.
-* */
-//  == checks whether two references point to the same object in memory.
-//It does not compare the actual characters.
-        String username = "Prathamesh";
-        String input = "Prathamesh";
+// ==
+// Compares the references (memory addresses) of two String objects.
+// Returns true if both variables point to the same object in memory.
 
-        System.out.println(username==input);
-        //.equals() compares the contents (characters) of two strings.
-        String a = "hi";
-        String b = "hello";
-        System.out.println(a.equals(b));
+
+// .equals()
+// Compares the actual contents (characters) of two Strings.
+// Returns true if both Strings contain exactly the same text.
+
+
+// .compareTo()
+// Compares two Strings lexicographically (dictionary/alphabetical order).
+// Returns:
+// 0  -> Strings are equal
+// < 0 -> Current String comes before the other String
+// > 0 -> Current String comes after the other String
+
+        String name = "Prathamesh";
+        String name1="Prathamesh";
+
+        System.out.println(name==name1);  // compare memory address
+        System.out.println(name.equals(name1)); // compare actual content
+        System.out.println(name.compareTo(name1));
+
+
+
+
+
     }
 }

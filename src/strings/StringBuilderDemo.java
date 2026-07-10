f73b7e87de65969d5f67439382de1511d4e74c3d
Prathamesh-07git <prathameshdeshmukh090@gmail.com>
2026-07-10 23:48:51 +0530

    String builder methods

--- src/strings/StringBuilderDemo.java
+++ src/strings/StringBuilderDemo.java
@@ -1,4 +1,19 @@
 package strings;
-
+//StringBuilder is a mutable sequence of characters.
+import java.util.*;
 public class StringBuilderDemo {
+
+    public static void main(String [] args){
+
+        StringBuilder sb = new StringBuilder("Java");
+
+        sb.append(" Programming");
+        System.out.println(sb);
+
+        System.out.println(sb.reverse());
+
+
+
+    }
+
 }
